bf95f45d3fb3d959f19b5c23f9ae2e99a4583040
Ian Hart <ianhart7790@gmail.com>
2024-04-26 10:38:50 -0400

    feat: create user model with one to many thoughts and friends relationships

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: [true, 'A username is required'],
+      trim: true,
+      lowercase: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'An email is required'],
+      match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please enter a valid email address'],
+      lowercase: true,
+    },
+
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
