727cce0b3982f3936932fb3d88ae75fb245d6d64
Ian Hart <ianhart7790@gmail.com>
2024-04-26 15:01:44 -0400

    feat: can now query a single thought by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,4 +9,20 @@ module.exports = {
       res.status(500).json({ message: err.message });
     }
   },
+
+  async getThought(req, res) {
+    try {
+      const thoughtId = req.params.thoughtId;
+
+      const thought = await Thought.findById(thoughtId);
+
+      if (!thought) {
+        return res.status(404).json({ message: `A thought with the id ${thoughtId} was not found` });
+      }
+
+      res.status(200).json({ message: 'success', data: thought });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
 };
--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-const { getAllThoughts } = require('../../controllers/thoughtController');
+const { getAllThoughts, getThought } = require('../../controllers/thoughtController');
 
+/* /api/thoughts  */
 router.route('/').get(getAllThoughts);
+
+/* api/thoughts/:thoughtId  */
+router.route('/:thoughtId').get(getThought);
 
 module.exports = router;
