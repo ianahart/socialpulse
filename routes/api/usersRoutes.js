54d2d7f126771a3e000259f08788826c9cb0b3c2
Ian Hart <ianhart7790@gmail.com>
2024-04-26 13:44:37 -0400

    feat: can now create a friend

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
-const { getAllUsers, getUser, createUser, updateUser, deleteUser } = require('../../controllers/userController');
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+} = require('../../controllers/userController');
 
 /* /api/users */
 router.route('/').get(getAllUsers).post(createUser);
 
 /* /api/users/:userId */
 router.route('/:userId').get(getUser).put(updateUser).delete(deleteUser);
+
+/* /api/users/:userId/friends/:friendId */
+router.route('/:userId/friends/:friendId').post(addFriend);
 
 module.exports = router;
